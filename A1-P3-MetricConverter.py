295059e3d3f5000753ba4b321065b7d9a48a0645
Your Name <you@example.com>
2024-10-06 20:09:37 -0300

    Program 3 of assignment 1 ready for marking. The program will run, but the calculations are incorrect, I spent most of Saturday and Sunday trying to figure it out.

--- A1-P3-MetricConverter.py
+++ A1-P3-MetricConverter.py
@@ -4,14 +4,42 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    #Equivalents in ounces
+    #Ton = 35840   Metric ton = 32000
+    #Stone = 224   kilogram = 35.274
+    #pound = 16    gram = 28.3495
+    #ounce = 1     milligram = 0.00003527
 
+    #User inputs values
+    TonS = (float(input("Enter number of tons:")))
+    StoneS = (float(input("Enter number of stones:")))
+    PoundS = (float(input("Enter number of pounds:")))
+    OunceS = (float(input("Enter number of ounces:")))
 
+    #Conversion calculations
+    TotalOunces = float((35840*TonS)+(224*StoneS)+(16*PoundS+OunceS))          #<--I can't get the math right, everything runs but the math is wrong.
 
+    TotalKilos = (int(TotalOunces/35.274))
 
+    MetricTons = (int(TotalKilos/1000))
+    #kilograms = (int(TotalKilos/100))
 
+    GramS = (float(TotalKilos/1000))
+    #grams2 = (float(GramS/100))
+    grams = ('{0:.1f} grams'.format(GramS))
 
+    #Outcome Output
+    Outcome = "The Metric weight is: {0} metric tons, {1} kilos, and {2} ".format(MetricTons,(int(TotalKilos)),grams)
 
+    print(Outcome)
+    #print("The Metric weight is:{0} metric tons, {1} kilos, and {2} grams".format((MetricTons,TotalKilos,GramS)))      #<-- First attempts
 
+    #print(int(MetricTons),"metric Tons,",end="")
+
+    #print(int(TotalKilos),"kilos, and",end="")
+
+    #print(float(GramS),'grams.{0:.2f}')
+    #print(float(GramS),"grams.")
     # YOUR CODE ENDS HERE
 
 main()
